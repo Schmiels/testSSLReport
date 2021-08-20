32aebc227afab4ea471e3d14671c06619d236ec9
Schmiels <svenbartos@gmail.com>
2021-08-20 13:08:44 -0400

    version filter added

--- check_ciphers.py
+++ check_ciphers.py
@@ -19,20 +19,25 @@ VERSIONS = ["SSLv2", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"]
 OUTPUTLOCATION = "./testData/output/"
 
 ### Helper
-def parseFile(fileType, fileName, dirName):
+def parseFile(fileType, fileName, dirName, versionFilter):
     # WORKAROUND
     f = open(dirName + fileName + fileType, "r")
     # OLD
     # f = open(fileName + fileType, "r")
     # DEBUG
-    # print(f.name)
-    # exit(0)
+    print(versionFilter)
     if fileType == ".html":
         cipherBuffer = ""
         writeToBuffer = False
         for line in f:
+            # TODO: schöner machen
             # Will only work as long as SSLv2 is listed by testssl.sh
-            if line.startswith(FILTER_START_TAG + VERSIONS[0] + FILTER_END_TAG):
+            if versionFilter != "ALL":
+                if line.startswith(FILTER_START_TAG + versionFilter + FILTER_END_TAG):
+                    writeToBuffer = True
+                elif line.startswith(FILTER_START_TAG) and not line.endswith(versionFilter + FILTER_END_TAG):
+                    writeToBuffer = False
+            elif line.startswith(FILTER_START_TAG + VERSIONS[0] + FILTER_END_TAG):
                 writeToBuffer = True
             elif line.strip() == "":
                 writeToBuffer = False
@@ -51,49 +56,50 @@ def parseFile(fileType, fileName, dirName):
     f.close()
     return cipherList 
 
-def evaluateCiphers(fileName, dirName):
+def evaluateCiphers(fileName, dirName, versionFilter):
     fName, fType = os.path.splitext(fileName)
-    cipherList = parseFile(fType, fName, dirName)
-
-    # TODO: Schöner machen
-    outputPrefix = re.search(r"([0-9]{1,3}\.){3}[0-9]{1,3}_p[0-9]{1,5}", fName)
-    fileNameError = False
-    if outputPrefix == None:
-        outputPrefix = re.search(r"([a-z0-9]*\.)*[a-z]*_p[0-9]{1,5}", fName)
+    cipherList = parseFile(fType, fName, dirName, versionFilter)
+    
+    if len(cipherList) > 0:
+        # TODO: Schöner machen
+        outputPrefix = re.search(r"([0-9]{1,3}\.){3}[0-9]{1,3}_p[0-9]{1,5}", fName)
+        fileNameError = False
         if outputPrefix == None:
-            fileNameError = True
+            outputPrefix = re.search(r"([a-z0-9]*\.)*[a-z]*_p[0-9]{1,5}", fName)
+            if outputPrefix == None:
+                fileNameError = True
 
-    if fileNameError:
-        print("Unsupported file name format for: " + fileName)
-        print("Processing for " + fileName + " canceled")
-        return None
-    else:
-        outputPrefix = outputPrefix.group()
-
-    weakF = open(OUTPUTLOCATION + outputPrefix + "_weak.txt", "w")
-    # TODO: enable via param 
-    # secureF = open(OUTPUTLOCATION + outputPrefix + "_secure.txt", "w")
-
-    for cipher in cipherList:
-        requestsUrl = API_URL + cipher + "/"
-        r = requests.get(requestsUrl)
-        if r.ok:
-            secVal = r.json()[cipher][DATA_PARAM]
-            if secVal == "insecure" or secVal == "weak":
-                weakF.write(cipher + "\n")
-            # TODO: enable via param
-            # elif secVal == "secure" or secVal == "recommended":
-            #    secureF.write(cipher + "\n")
-            else:
-                print("Unsupported " + DATA_PARAM + " value: " + secVal)
+        if fileNameError:
+            print("Unsupported file name format for: " + fileName)
+            print("Processing for " + fileName + " canceled")
+            return None
         else:
-            print("Request failed for cipher:" + cipher)
-            print(r.status_code)
-            print(r.text)
+            outputPrefix = outputPrefix.group()
 
-    weakF.close()
-    # TODO: enable via param
-    # secureF.close()
+        weakF = open(OUTPUTLOCATION + outputPrefix + "_weak.txt", "w")
+        # TODO: enable via param 
+        # secureF = open(OUTPUTLOCATION + outputPrefix + "_secure.txt", "w")
+
+        for cipher in cipherList:
+            requestsUrl = API_URL + cipher + "/"
+            r = requests.get(requestsUrl)
+            if r.ok:
+                secVal = r.json()[cipher][DATA_PARAM]
+                if secVal == "insecure" or secVal == "weak":
+                    weakF.write(cipher + "\n")
+                # TODO: enable via param
+                # elif secVal == "secure" or secVal == "recommended":
+                #    secureF.write(cipher + "\n")
+                else:
+                    print("Unsupported " + DATA_PARAM + " value: " + secVal)
+            else:
+                print("Request failed for cipher:" + cipher)
+                print(r.status_code)
+                print(r.text)
+
+        weakF.close()
+        # TODO: enable via param
+        # secureF.close()
 
 ### Main
 if __name__ == "__main__":
@@ -105,18 +111,28 @@ if __name__ == "__main__":
         # TODO: Help-Output
         # if sys.argv[1] == "-h" | "--help"
         #    print("")
+        # TODO: parse parameters => execute evaluateCiphers
+        # Filter SSL/TLS Version
+        if "v" in sys.argv:
+            versionFilter = sys.argv[sys.argv.index("v")+1]
+            if versionFilter not in VERSIONS:
+                print("Unsupported SSL/TLS Version: " + versionFilter)
+                exit(0)
+        else:
+            versionFilter = "ALL"
         # Scan given directory
-        if sys.argv[1] == "d":
-            dirName = sys.argv[2]
+        if "d" in sys.argv:
+            dirName = sys.argv[sys.argv.index("d")+1]
 
             for f in os.listdir(dirName):
                 # TODO: Verschiedene FileTypes implementieren (json)
                 if f.endswith(".html"):
                     print("Processing: " + f)
-                    evaluateCiphers(f, dirName)
-        elif sys.argv[1] == "f":
-            fileName = sys.argv[2]
+                    evaluateCiphers(f, dirName, versionFilter)
+        elif "f" in sys.argv:
+            # TODO: muss wahrscheinlich überarbeitet werden
+            fileName = sys.argv[sys.argv.index("f")+1]
             print("Processing: " + fileName) 
-            evaluateCiphers(f)
+            evaluateCiphers(f, dirName, versionFilter)
     else:
         print("Invalid arguments")
