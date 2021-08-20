09bf324e2919be2beb07f53af0ab39b0834e8933
Schmiels <svenbartos@gmail.com>
2021-08-20 12:29:44 -0400

    major changes: disabled secure listing, added input dir; removed and added some debugging lines

--- check_ciphers.py
+++ check_ciphers.py
@@ -16,10 +16,17 @@ DATA_PARAM = "security"
 FILTER_START_TAG = "<u>"
 FILTER_END_TAG = "</u>"
 VERSIONS = ["SSLv2", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"]
+OUTPUTLOCATION = "./testData/output/"
 
 ### Helper
-def parseFile(fileType, fileName):
-    f = open(fileName + fileType, "r")
+def parseFile(fileType, fileName, dirName):
+    # WORKAROUND
+    f = open(dirName + fileName + fileType, "r")
+    # OLD
+    # f = open(fileName + fileType, "r")
+    # DEBUG
+    # print(f.name)
+    # exit(0)
     if fileType == ".html":
         cipherBuffer = ""
         writeToBuffer = False
@@ -44,9 +51,9 @@ def parseFile(fileType, fileName):
     f.close()
     return cipherList 
 
-def evaluateCiphers(fileName):
+def evaluateCiphers(fileName, dirName):
     fName, fType = os.path.splitext(fileName)
-    cipherList = parseFile(fType, fName)
+    cipherList = parseFile(fType, fName, dirName)
 
     # TODO: Schöner machen
     outputPrefix = re.search(r"([0-9]{1,3}\.){3}[0-9]{1,3}_p[0-9]{1,5}", fName)
@@ -54,7 +61,6 @@ def evaluateCiphers(fileName):
     if outputPrefix == None:
         outputPrefix = re.search(r"([a-z0-9]*\.)*[a-z]*_p[0-9]{1,5}", fName)
         if outputPrefix == None:
-            print("zweiter Versuch")
             fileNameError = True
 
     if fileNameError:
@@ -64,30 +70,30 @@ def evaluateCiphers(fileName):
     else:
         outputPrefix = outputPrefix.group()
 
-    weakF = open("output/" + outputPrefix + "_weak.txt", "w")
-    secureF = open("output/" + outputPrefix + "_secure.txt", "w")
+    weakF = open(OUTPUTLOCATION + outputPrefix + "_weak.txt", "w")
+    # TODO: enable via param 
+    # secureF = open(OUTPUTLOCATION + outputPrefix + "_secure.txt", "w")
 
     for cipher in cipherList:
         requestsUrl = API_URL + cipher + "/"
         r = requests.get(requestsUrl)
         if r.ok:
             secVal = r.json()[cipher][DATA_PARAM]
-            # DEBUG
-            print(secVal)
             if secVal == "insecure" or secVal == "weak":
-                print(cipher + ":" + secVal)
                 weakF.write(cipher + "\n")
-            elif secVal == "secure" or secVal == "recommended":
-                secureF.write(cipher + "\n")
+            # TODO: enable via param
+            # elif secVal == "secure" or secVal == "recommended":
+            #    secureF.write(cipher + "\n")
             else:
-                print("Unsupported " + DATA_PARAM + " value")
+                print("Unsupported " + DATA_PARAM + " value: " + secVal)
         else:
             print("Request failed for cipher:" + cipher)
             print(r.status_code)
             print(r.text)
 
     weakF.close()
-    secureF.close()
+    # TODO: enable via param
+    # secureF.close()
 
 ### Main
 if __name__ == "__main__":
@@ -107,7 +113,7 @@ if __name__ == "__main__":
                 # TODO: Verschiedene FileTypes implementieren (json)
                 if f.endswith(".html"):
                     print("Processing: " + f)
-                    evaluateCiphers(f)
+                    evaluateCiphers(f, dirName)
         elif sys.argv[1] == "f":
             fileName = sys.argv[2]
             print("Processing: " + fileName) 
